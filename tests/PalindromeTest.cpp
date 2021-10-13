fa2fa8bec8f77805e17936713ecb253250518569
Michaël AZERHAD - WealCome <mazerhad@wealcomecompany.com>
2021-10-13 14:33:07 +0200

    add main starter for tests

--- tests/PalindromeTest.cpp
+++ tests/PalindromeTest.cpp
@@ -1,5 +1,10 @@
 #include "gmock/gmock.h"
 
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
 TEST(PalindromeCheckerSuite, DummyTestForExample) {
     EXPECT_THAT(1, testing::Eq(1));
 }
